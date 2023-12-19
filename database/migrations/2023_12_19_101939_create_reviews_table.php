80e40e0a0dd882419a15314ad75fead574075348
dimaspng <d.sagachko@gmail.com>
2023-12-19 12:49:37 +0200

    Created new Review model with relationships and migration

---
+++ database/migrations/2023_12_19_101939_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('author_name');
+            $table->text('comment');
+            $table->integer('rating');
+            $table->boolean('moderation_status')->default(false);
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('user_id');
+            $table->index('product_id', 'product_idx');
+            $table->index('user_id', 'user_idx');
+            $table->foreign('product_id', 'product_id_fk')->on('products')->references('id');
+            $table->foreign('user_id', 'user_id_fk')->on('users')->references('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
