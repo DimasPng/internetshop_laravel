4e7c5ad8f58629c17fd720dbd7021bd00e21164d
dimaspng <d.sagachko@gmail.com>
2023-12-19 12:58:50 +0200

    Added a StoreController for Rewievs

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -13,4 +13,15 @@ class Review extends Model
 
     protected $table = 'reviews';
     public $guarded = false;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
 }
